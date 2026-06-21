028df0c08dbf5b462e3990d91041b09353c113ec
blukeryellow-arch <blukeryellow@gmail.com>
2026-06-21 14:55:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ app.listen(process.env.PORT || 3000);
 console.log("--- URUCHAMIAM BOTA ---");
 
 const bot = mineflayer.createBot({
-  host: 'KakaszkavivalSMP.aternos.me', // Sama domena, bez portu na końcu
-  port: 22772,                   // Port podajemy tutaj, oddzielnie
+  host: 'kinkajou.aternos.me',
+  port: 25565,
   username: 'BotAktywny',
   version: '1.21.11',
   skipValidation: true
 });
 
-bot.on('spawn', () => console.log("Bot jest w grze!"));
-bot.on('error', (err) => console.log("BŁĄD:", err));
+console.log("--- POLECENIE CREATEBOT WYKONANE ---");
+
+bot.on('spawn', () => console.log("Bot dołączył do gry!"));
+bot.on('login', () => console.log("Bot zalogowany!"));
+bot.on('error', (err) => console.log("BŁĄD BOTA:", err));
 bot.on('kicked', (reason) => console.log("WYRZUCONO:", reason));
+bot.on('end', (reason) => console.log("POŁĄCZENIE ZAKOŃCZONE:", reason));
