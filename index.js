ac0323af0ccb0fad04bde2eefb6d93c11c45ab0d
blukeryellow-arch <blukeryellow@gmail.com>
2026-06-21 14:31:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const bot = mineflayer.createBot({
 });
 
 console.log("--- PRÓBA POŁĄCZENIA Z SERWEREM ---");
+const dns = require('dns');
+dns.lookup('KakaszkavivalSMP.aternos.me', (err, address) => {
+  if (err) console.log("BŁĄD DNS (nie można znaleźć serwera):", err);
+  else console.log("IP serwera znalezione:", address);
+});
 
 bot.on('spawn', () => console.log("Bot dołączył do gry!"));
 bot.on('error', (err) => console.log("BŁĄD BOTA:", err)); // TO POKAŻE CO JEST NIE TAK
