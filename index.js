aba59aed5c8365807bd9b22d7ba497445f4a1785
blukeryellow-arch <blukeryellow@gmail.com>
2026-06-21 14:38:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const app = express();
 app.get('/', (req, res) => res.send('Bot działa!'));
 app.listen(process.env.PORT || 3000);
 
-console.log("--- BOT SIĘ URUCHAMIA ---");
+function connect() {
+  const bot = mineflayer.createBot({
+    host: 'kinkajou.aternos.host',
+    port: 22772,
+    username: 'BotAktywny',
+    version: '1.21.11',
+    skipValidation: true
+  });
 
-const bot = mineflayer.createBot({
-  host: '185.107.193.195', // Zmień na to IP
-  port: 22772,
-  username: 'BotAktywny',
-  version: 1.21.11,
-  skipValidation: true
-});
+  bot.on('spawn', () => console.log("Bot dołączył do gry!"));
+  bot.on('error', (err) => {
+    console.log("BŁĄD BOTA:", err);
+    setTimeout(connect, 10000); // Czekaj 10 sekund przed ponowną próbą
+  });
+  bot.on('kicked', (reason) => {
+    console.log("WYRZUCONO:", reason);
+    setTimeout(connect, 10000);
+  });
+}
 
-console.log("--- PRÓBA POŁĄCZENIA Z SERWEREM ---");
-const dns = require('dns');
-dns.lookup('KakaszkavivalSMP.aternos.me', (err, address) => {
-  if (err) console.log("BŁĄD DNS (nie można znaleźć serwera):", err);
-  else console.log("IP serwera znalezione:", address);
-});
-
-bot.on('spawn', () => console.log("Bot dołączył do gry!"));
-bot.on('error', (err) => console.log("BŁĄD BOTA:", err)); // TO POKAŻE CO JEST NIE TAK
-bot.on('kicked', (reason) => console.log("WYRZUCONO Z SERWERA:", reason));
-bot.on('end', (reason) => console.log("POŁĄCZENIE ZAKOŃCZONE:", reason));
+connect();
