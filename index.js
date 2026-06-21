b974e25ce39865f6a332e5afb7df987dad6e16da
blukeryellow-arch <blukeryellow@gmail.com>
2026-06-21 13:48:52 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const mineflayer = require('mineflayer');
+const express = require('express');
+const app = express();
+
+// Serwer HTTP, żeby Render nie uśpił bota
+app.get('/', (req, res) => res.send('Bot działa!'));
+app.listen(process.env.PORT || 3000);
+
+const bot = mineflayer.createBot({
+  host: 'TWOJ_SERWER.aternos.me', // ZMIEŃ NA ADRES SWOJEGO SERWERA
+  port: 25565,
+  username: 'BotAktywny',
+  version: false
+});
+
+// Zabezpieczenie przed AFK
+setInterval(() => {
+  bot.look(bot.entity.yaw + 1, 0);
+  console.log("Bot wykonał ruch.");
+}, 300000); // co 5 minut
+
+bot.on('spawn', () => console.log("Bot dołączył do gry!"));
+bot.on('error', (err) => console.log("Błąd:", err));
+bot.on('kicked', (reason) => console.log("Wyrzucono:", reason));
